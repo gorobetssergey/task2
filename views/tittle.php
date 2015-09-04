461a2c130ee4d86e4dd8167952168f208e67ece8
gorobetssergey <momona@ukr.net>
2015-09-04 17:35:30 +0300

    вивів через класи список новин

---
+++ views/tittle.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sergiy
+ * Date: 04.09.2015
+ * Time: 17:25
+ */
+
+class Tittle {
+        private $tittle='Новина';
+        private $data='Дата';
+    public function tittle()
+    {
+        return $this->tittle;
+    }
+    public function data()
+    {
+        return $this->data;
+    }
+
+} 
